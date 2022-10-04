98624dca6af835aed1048f52e0682be06c67a45f
Nico <no email provided>
2022-10-04 10:53:37 +0200

    fix: 🐛 constants SSAJSON

--- packages/nextjs-app-ts/src/helpers/constants.ts
+++ packages/nextjs-app-ts/src/helpers/constants.ts
@@ -1,6 +1,6 @@
 import hardhatDeployedContractsJson from '~common/generated/hardhat_contracts.json'
 
-export const SSAJson = hardhatDeployedContractsJson[5][0].contracts.Subscription_SuperApp as {
+export const SSAJson = hardhatDeployedContractsJson[80001][0].contracts.Subscription_SuperApp as {
   address: string
   abi: []
 }
